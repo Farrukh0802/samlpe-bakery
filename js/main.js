7d1e0939dc4732449cd4bf4af5f6b7212e6ff262
Farrukh0802 <Farrukh0802@users.noreply.github.com>
2021-01-22 18:24:00 +0500

    add files

--- js/main.js
+++ js/main.js
@@ -128,6 +128,36 @@ $(document).ready(function(){
     });
     $('.carousel').carousel({
       interval: 2000
-    })
-
-    
+    });
+    $(document).ready(function(){
+      $('.slider').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    });
